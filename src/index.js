15951031eedf54b369c7a8213db06af8b7f62e1c
Lorn Samnang <samnanglorn@gmail.com>
2022-10-09 13:59:30 +0700

    add and configure sass and admin lte

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+/* ----------- Admin LTE ----------- */
+import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
+import 'admin-lte/dist/css/adminlte.min.css';
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
+
+import './style.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
